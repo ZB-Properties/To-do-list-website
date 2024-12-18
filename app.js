3b1089f62a749311d0d79b0fa3b8a1dc98f868d4
Shope Alli-B <zoeyalli921@gmail.com>
2024-12-18 01:30:08 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+let items = ["Get foodstuffs", "Cook the preferred meal of choice", "Serve and Eat the meal"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+    let day = date();
+
+    res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "item"){
+    items.push(item);
+    res.redirect("/");
+  } else {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+
+app.listen(4000, function(){
+    console.log("Server running on port 4000");
+})

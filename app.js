ae96ed64582b557262d06c4a9230f99c951f2993
Shope Alli-B <zoeyalli921@gmail.com>
2024-12-18 12:11:30 -0800

    first commit

--- app.js
+++ app.js
@@ -26,18 +26,19 @@ app.post("/", function(req, res){
 
   let item = req.body.newItem;
 
-  if (req.body.list === "item"){
-    items.push(item);
-    res.redirect("/");
-  } else {
+  if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
   }
+
 
 });
 
 app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", newListItems: workItems})
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 
